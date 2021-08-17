3bf63e7ecd4417c13f8f6ed1a242f2f3b96a9c75
Garrett Hyde <ghyde@users.noreply.github.com>
2021-08-17 17:24:25 -0600

    Update Academic theme to v5.3.0

--- scripts/update_template.sh
+++ scripts/update_template.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
+# Delete the Hugo Module cache for the current project
 hugo mod clean
 
-# Latest development release
-#hugo mod get -u ./...
+# Install the latest versions of all module dependencies
+hugo mod get -u
 
-# Latest official release
-#hugo mod get -u
-
-# Specific git commit
-GIT_COMMIT="fda9f39"
-hugo mod get github.com/wowchemy/wowchemy-hugo-modules/wowchemy@${GIT_COMMIT}
-hugo mod get github.com/wowchemy/wowchemy-hugo-modules/wowchemy-cms@${GIT_COMMIT}
-hugo mod get github.com/wowchemy/wowchemy-hugo-modules/netlify-cms-academic@${GIT_COMMIT}
-
+# Remove unused entries in go.mod and go.sum
 hugo mod tidy
